ec19a6f439fce9d117314733f0d4e03b53106300
yangyifan <yyf919@outlook.com>
2019-03-29 23:35:06 -0400

    fix a bug in last commit

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from loss import PGLoss
 
 # Arguemnts
 parser = argparse.ArgumentParser(description='SeqGAN')
-parser.add_argument('--hpc', action='store_true', default=True,
+parser.add_argument('--hpc', action='store_true', default=False,
                     help='set to hpc mode')
 parser.add_argument('--data_path', type=str, default='dataset/', metavar='PATH',
                     help='data path to save files (default: dataset/)')
@@ -78,7 +78,7 @@ d_num_filters = [100, 200, 200, 200, 200, 100, 100, 100, 100, 100, 160, 160]
 d_dropout_prob = 0.2
 
 
-def generate_samples(model, batch_size, generated_num, output_file, ad_train=False, EPOCH_FILE=''):
+def generate_samples(model, batch_size, generated_num, output_file, ad_train=False, epoch_file=''):
     samples = []
     for _ in range(int(generated_num / batch_size)):
         sample = model.sample(batch_size, g_seq_len).cpu().data.numpy().tolist()
@@ -90,7 +90,7 @@ def generate_samples(model, batch_size, generated_num, output_file, ad_train=Fal
             string = ' '.join([str(s) for s in sample])
             fout.write('%s\n' % string)
     if ad_train:
-        with open(output_file, 'a') as fout:
+        with open(epoch_file, 'a') as fout:
             for i, sample in enumerate(samples):
                 if i > 9: break
                 # string = ''.join([str(s) for s in sample])
@@ -289,7 +289,7 @@ if __name__ == '__main__':
     # print('Generating data ...')
     # print('#####################################################\n\n')
     # generate_samples(target_lstm, args.batch_size, args.n_samples, POSITIVE_FILE)
-
+    """
     # Pre-train generator using MLE
     print('#####################################################')
     print('Start pre-training generator with MLE...')
@@ -321,7 +321,7 @@ if __name__ == '__main__':
         dis_pretrain_eval_acc.append(dis_acc)
         print("eval loss: {:.5f}, eval acc: {:.3f}\n".format(dis_loss, dis_acc))
     print('#####################################################\n\n')
-
+    """
     # Adversarial training
     print('#####################################################')
     print('Start adversarial training...')
@@ -332,7 +332,7 @@ if __name__ == '__main__':
         adversarial_train(generator, discriminator, rollout, 
             pg_loss, nll_loss, gen_optimizer, dis_optimizer, 
             dis_adversarial_train_loss, dis_adversarial_train_acc, args)
-        generate_samples(generator, args.batch_size, args.n_samples, NEGATIVE_FILE, ad_train=True, EPOCH_FILE=EPOCH_FILE)
+        generate_samples(generator, args.batch_size, args.n_samples, NEGATIVE_FILE, ad_train=True, epoch_file=EPOCH_FILE)
         gen_eval_iter = GenDataIter(NEGATIVE_FILE, args.batch_size)
         dis_eval_iter = DisDataIter(POSITIVE_FILE, NEGATIVE_FILE, args.batch_size)
         gen_loss = eval_generator(target_lstm, gen_eval_iter, nll_loss, args)
