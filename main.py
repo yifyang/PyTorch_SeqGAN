db3fa26cd08e497c9e8b26f0c2843f007796bea1
yangyifan <yyf919@outlook.com>
2019-04-15 20:42:06 -0400

    reset a parameter

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ parser.add_argument('--data_path', type=str, default='dataset/', metavar='PATH',
                     help='data path to save files (default: dataset/)')
 parser.add_argument('--rounds', type=int, default=200, metavar='N',
                     help='rounds of adversarial training (default: 150)')
-parser.add_argument('--g_pretrain_steps', type=int, default=350, metavar='N',
+parser.add_argument('--g_pretrain_steps', type=int, default=300, metavar='N',
                     help='steps of pre-training of generators (default: 120)')
 parser.add_argument('--d_pretrain_steps', type=int, default=50, metavar='N',
                     help='steps of pre-training of discriminators (default: 50)')
