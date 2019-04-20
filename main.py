a3f208d8c3f837a589edf7c145c7ce41f28d3af7
yangyifan <yyf919@outlook.com>
2019-04-20 16:59:56 -0400

    try it on plot data

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ parser.add_argument('--update_rate', type=float, default=0.8, metavar='UR',
                     help='update rate of roll-out model (default: 0.8)')
 parser.add_argument('--n_rollout', type=int, default=3, metavar='N',
                     help='number of roll-out (default: 16)')
-parser.add_argument('--vocab_size', type=int, default=21, metavar='N',
+parser.add_argument('--vocab_size', type=int, default=10, metavar='N',
                     help='vocabulary size (default: 28261, 7521)')
 parser.add_argument('--batch_size', type=int, default=64, metavar='N',
                     help='batch size (default: 64)')
@@ -63,10 +63,10 @@ parser.add_argument('--seq_len', type=int, default=20, metavar='S',
 
 
 # Files
-POSITIVE_FILE = 'self_noz.data'
-NEGATIVE_FILE = 'gen_self_noz.data'
-RANDOM_FILE = 'self_rand_noz.data'
-EPOCH_FILE = 'epoch_self_noz.data' # store samples every epoch during adversarial training
+POSITIVE_FILE = 'plot_0420.data'
+NEGATIVE_FILE = 'gen_plot.data'
+RANDOM_FILE = 'plot_rand.data'
+EPOCH_FILE = 'epoch_plot_0420.data' # store samples every epoch during adversarial training
 
 # Genrator Parameters
 g_embed_dim = 64
@@ -376,13 +376,13 @@ if __name__ == '__main__':
     dis_adversarial_train_acc = []
     dis_adversarial_eval_loss = []
     dis_adversarial_eval_acc = []
-    """
+
     # Generate toy data using target LSTM
     print('#####################################################')
     print('Generating data ...')
     print('#####################################################\n\n')
     generate_samples(target_lstm, args.batch_size, args.n_samples, POSITIVE_FILE, toy_data=True)
-    """
+
     # Pre-train generator using MLE
     print('#####################################################')
     print('Start pre-training generator with MLE...')
